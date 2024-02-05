fb8470884eab3d231f45da2c6fa3803d9e157ad7
Faramarz.B <61977061+Faramarz-bagherzadeh@users.noreply.github.com>
2024-02-05 15:59:52 +0100

    Add files via upload

---
+++ DeepLearning/dataloader.py
@@ -0,0 +1,131 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import ToTensor, RandomHorizontalFlip, RandomVerticalFlip
+import numpy as np
+from torchvision import transforms
+import tifffile
+from patchify import patchify, unpatchify
+import kornia
+import skimage
+import cv2
+import random
+
+from augmentation import MyAugmentationPipeline
+
+rng = torch.manual_seed(0)
+# Defining dataset
+class microCT_Dataset(Dataset):
+
+        
+    def __init__(self, data_path, target_path, HR_patch_size, transform):
+        
+        self.patch_sizeHR = HR_patch_size
+        self.patch_sizeLR = int(self.patch_sizeHR*2)# patch_size
+        
+        self.stepHR = HR_patch_size
+        self.stepLR = HR_patch_size
+        
+        # reading data and dropping top and bottom layers
+        # these layers are empty becasue of registration 
+        self.data = tifffile.imread(data_path)[10:-10,:,:]
+        self.target = tifffile.imread(target_path)[10:-10,:,:]
+        
+        print ('reading data shape = ', self.data.shape)
+        print ('reading data max = ', self.data.max())
+        
+        # contrast stretching to avoid high intensity artifacts
+        self.data = self.contrast_stretching(self.data)
+        
+        #padding inputdata to a proper shape by adding half of the 
+        # difference between HR and LR patch size to each side of input
+        pd = int((self.patch_sizeLR-self.patch_sizeHR)/2)
+        #print ('padding amount = ',pd)
+        self.data = np.pad(self.data, ((pd,pd), (pd,pd), (pd,pd)), mode='constant') 
+
+        
+        self.transform = transform
+
+      
+        # Data and target into patches
+
+        self.data = self.patchyfy_img(self.data,self.patch_sizeLR,self.stepLR)
+        self.target = self.patchyfy_img(self.target,self.patch_sizeHR,self.stepHR)
+        
+        
+        print ('out_of_loader data shape = ', self.data.shape)
+        print ('out_of_loader data max = ', self.data.max())
+
+        print ('out_of_loader target shape = ', self.target.shape)
+        print ('out_of_loader target max = ', self.target.max())
+        
+        
+        
+    def patchyfy_img(self,img, ps, step):
+        img = patchify(img,(ps, ps, ps) ,  step=step )
+        print (img.shape)
+        img = img.reshape(img.shape[0]*img.shape[1]*img.shape[2],ps,ps,ps )
+        return img
+
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def contrast_stretching(self,input_image):
+        # Contrast stretching
+        # Dropping extreems (artifacts)
+        p2, p98 = np.percentile(input_image, (2, 98))
+        stretched_image = skimage.exposure.rescale_intensity(input_image, in_range=(p2, p98))
+        return stretched_image
+    
+    def __getitem__(self, index):
+
+        data = torch.from_numpy(self.data[index]).unsqueeze(0).float()
+        target = torch.from_numpy(self.target[index]).unsqueeze(0).float()
+        
+        
+        
+
+        
+        
+        if self.transform is not None:
+            
+            
+            aug = MyAugmentationPipeline()
+            data , target = aug.forward(data, target)
+            
+            #print ('data ot of transformation max = ', data.max())
+            # Expanding dimension for batching
+
+
+        #target = torch.cat(((target<0.5),(target>0.5)), dim=0).float()
+        #print ('final target shape', target.shape)
+        
+        return data, target
+
+
+'''class ConcatDataset(torch.utils.data.Dataset):
+    def __init__(self, *datasets):
+        self.datasets = datasets
+
+    def __getitem__(self, i):
+        return tuple(d[i] for d in self.datasets)
+
+    def __len__(self):
+        return min(len(d) for d in self.datasets)
+def train_dataloader(self):
+        concat_dataset = ConcatDataset(
+            datasets.ImageFolder(traindir_A),
+            datasets.ImageFolder(traindir_B)
+        )
+
+        loader = torch.utils.data.DataLoader(
+            concat_dataset,
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=args.workers,
+            pin_memory=True
+        )
+        '''
