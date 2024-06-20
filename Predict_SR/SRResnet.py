00f88e9dcbd764ad9bfb0ee598cb3724c4e1113a
Faramarz.B <61977061+Faramarz-bagherzadeh@users.noreply.github.com>
2024-06-20 15:18:53 +0200

    Add files via upload

--- Predict_SR/SRResnet.py
+++ Predict_SR/SRResnet.py
@@ -1,32 +1,32 @@
 import torch
 import torch.nn as nn
-#from torchsummary import summary
+
 
 # Define the basic residual block
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=stride, padding='same', bias=False)
-        self.bn1 = nn.BatchNorm3d(out_channels)
+       # self.bn1 = nn.BatchNorm3d(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=3, stride=1, padding='same', bias=False)
-        self.bn2 = nn.BatchNorm3d(out_channels)
+       # self.bn2 = nn.BatchNorm3d(out_channels)
         
         # Shortcut connection to handle different dimensions
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
                 nn.Conv3d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
+               # nn.BatchNorm2d(out_channels)
             )
 
     def forward(self, x):
         residual = x
         out = self.conv1(x)
-        out = self.bn1(out)
+      #  out = self.bn1(out)
         out = self.relu(out)
         out = self.conv2(out)
-        out = self.bn2(out)
+       # out = self.bn2(out)
         out += self.shortcut(residual)
         out = self.relu(out)
         return out
@@ -43,7 +43,7 @@ class SRResNet(nn.Module):
         
         # Additional convolution layers
         self.conv2 = nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn = nn.BatchNorm3d(64)
+        #self.bn = nn.BatchNorm3d(64)
         
         # Upscaling layer
         #self.upscale = nn.Sequential(
@@ -62,30 +62,31 @@ class SRResNet(nn.Module):
         out = self.residual_blocks(out)
         
         out = self.conv2(out)
-        out = self.bn(out)
+        #out = self.bn(out)
         out += residual
         
         #out = self.upscale(out)
         out = self.conv3(out)
-        
+        zoom = int(out.shape[-1]/4)
+        out = out[:,:,zoom:-zoom,zoom:-zoom,zoom:-zoom]        
         return out
 
 
 
 
 
-
+from torchsummary import summary
 # Assuming your model is currently on the GPU
 #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Creating an instance of the model
-#model = SRResNet()
+model = SRResNet()
 #model = ResidualBlock(1, 1)
 # Move the model to the CPU
-#model.to('cpu')
+model.to('cpu')
 #     Print the model summary
 
-#summary(model, input_size=(1, 16, 16, 16), device="cpu")
+summary(model, input_size=(1, 16, 16, 16), device="cpu")
 
 
 
